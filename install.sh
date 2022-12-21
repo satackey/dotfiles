4ebe6ea9b24461e2a6b8f5e25dbea40a06d88435
satackey <21271711+satackey@users.noreply.github.com>
2022-12-21 14:30:24 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 if ! type "apt-get" > /dev/null; then
     apt-get install -y zsh
+    chsh -s /usr/bin/zsh
 fi
 
 git submodule update --init --recursive
 shopt -s extglob
-ln -fs dotfiles/.!(.|git|gitmodules|gitignore|) .
+ln -fs .!(.|git|gitmodules|gitignore|) ~
